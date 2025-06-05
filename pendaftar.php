4b2c8d41a0b817c63deed06debe6d90715eef036
Samantaslair <anantasamuel96@gmail.com>
2025-06-05 18:00:03 +0700

    Deadlineeeee rawrrrrr

--- pendaftar.php
+++ pendaftar.php
@@ -1,7 +1,6 @@
 <?php
 session_start(); 
 
-// // Cek autentikasi (aktifkan jika sudah ada sistem login yang berfungsi)
 // if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 //     header('Location: login.php');
 //     exit;
